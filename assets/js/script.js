3cdefa9449284a428de8096baeeffabda1132e23
abdelsurya <abdelsurya@gmail.com>
2022-11-22 17:37:41 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+  $(".mouse-laymas").css("cursor", "pointer");
+});
+
+$(document).ready(function () {
+  $(".mouse-laymas").click(function () {
+    $("#myModal").modal("toggle");
+  });
+});
+
+$(document).ready(function () {
+  $(".slideclick1").css("cursor", "pointer");
+});
+$(document).ready(function () {
+  $(".slideclick1").click(function () {
+    $(".up1").toggleClass("bi bi-chevron-up");
+  });
+});
+
+$(document).ready(function () {
+  $(".slideclick2").css("cursor", "pointer");
+});
+$(document).ready(function () {
+  $(".slideclick2").click(function () {
+    $(".up2").toggleClass("bi bi-chevron-up");
+  });
+});
+
+$(document).ready(function () {
+  $(".slideclick3").css("cursor", "pointer");
+});
+$(document).ready(function () {
+  $(".slideclick3").click(function () {
+    $(".up3").toggleClass("bi bi-chevron-up");
+  });
+});
+
+$(document).ready(function () {
+  $(".slideclick4").css("cursor", "pointer");
+});
+$(document).ready(function () {
+  $(".slideclick4").click(function () {
+    $(".up4").toggleClass("bi bi-chevron-up");
+  });
+});
+
+$(document).ready(function () {
+  $(".headingjkk").click(function () {
+    $(".collapsejkk").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".headingjkm").click(function () {
+    $(".collapsejkm").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".headingjht").click(function () {
+    $(".collapsejht").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".headingjpn").click(function () {
+    $(".collapsejpn").slideToggle("slow");
+  });
+});
+$(document).ready(function () {
+  $(".headingsimulasi").click(function () {
+    $(".collapsesimulasi").slideToggle("slow");
+  });
+});
